d053d13368ba1e4065b83b93f02c4582de97e074
jwang02 <jwang02@LinuxLite32>
2017-03-14 21:42:31 -0400

    Added usingSQLite3 and usingapscheduler (incompleted)

---
+++ usingapscheduler/usingapscheduler.py
@@ -0,0 +1,12 @@
+'''
+Created on Mar 14, 2017
+
+@author: jwang02
+'''
+# import sys
+# from time import sleep
+#  
+# ##############################################################
+#  
+# if __name__ == "__main__":
+#     main()
